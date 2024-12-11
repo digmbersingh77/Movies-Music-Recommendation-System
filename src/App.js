b69b1fbb66ed592f1b7c1b3c451ff917a912ed6a
Deepesh Gupta <108192513+deepeshmlgupta@users.noreply.github.com>
2024-12-11 09:07:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
-        navigate('/');
+        navigate('/Movies');
       } else {
         navigate('/SignIn');
       }
